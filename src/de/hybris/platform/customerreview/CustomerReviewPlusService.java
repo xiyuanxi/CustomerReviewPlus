b7a00f5e9663b1ca3571afe1346693baf46c641c
xiyuanxi <xiyuanxi@yahoo.com>
2017-12-11 12:23:26 -0500

    changes

--- src/de/hybris/platform/customerreview/CustomerReviewPlusService.java
+++ src/de/hybris/platform/customerreview/CustomerReviewPlusService.java
@@ -3,6 +3,6 @@ package de.hybris.platform.customerreview;
 import de.hybris.platform.core.model.product.ProductModel;
 
 public interface CustomerReviewPlusService {
-	 // get a product¡¯s total number of customer reviews whose ratings are within a given range (inclusive)
+	 // get a product's total number of customer reviews whose ratings are within a given range (inclusive)
 	 public abstract Integer getNumberOfReviewsForGivenRatingRange(ProductModel paramProductModel, Double paramDouble1,Double paramDouble2);
 }
--- src/de/hybris/platform/customerreview/jalo/GeneratedCustomerReviewPlusManager.java
+++ src/de/hybris/platform/customerreview/jalo/GeneratedCustomerReviewPlusManager.java
@@ -1,38 +1,15 @@
  package de.hybris.platform.customerreview.jalo;
  
- import de.hybris.platform.core.Tenant;
- import de.hybris.platform.customerreview.constants.GeneratedCustomerReviewConstants.Attributes.User;
- import de.hybris.platform.customerreview.constants.GeneratedCustomerReviewConstants.TC;
- import de.hybris.platform.jalo.Item;
- import de.hybris.platform.jalo.Item.AttributeMode;
- import de.hybris.platform.jalo.JaloBusinessException;
- import de.hybris.platform.jalo.JaloConnection;
- import de.hybris.platform.jalo.JaloInvalidParameterException;
- import de.hybris.platform.jalo.JaloSession;
- import de.hybris.platform.jalo.JaloSystemException;
  import de.hybris.platform.jalo.SessionContext;
  import de.hybris.platform.jalo.extension.Extension;
  import de.hybris.platform.jalo.product.Product;
- import de.hybris.platform.jalo.type.ComposedType;
- import de.hybris.platform.jalo.type.JaloGenericCreationException;
- import de.hybris.platform.jalo.type.TypeManager;
- import de.hybris.platform.jalo.user.User;
- import de.hybris.platform.util.OneToManyHandler;
- import java.util.Collection;
- import java.util.HashMap;
- import java.util.Map;
- 
- 
  
  public abstract class GeneratedCustomerReviewPlusManager
    extends Extension
  {
 
-   public abstract Integer getNumberOfReviewsForGivenRatingRange(SessionContext paramSessionContext, Product paramProduct,  Double paramDouble1, 	Double paramDouble2);
+	 public abstract Integer getNumberOfReviewsForGivenRatingRange(SessionContext paramSessionContext, Product paramProduct,  Double paramDouble1, 	Double paramDouble2);
    
- 
- 
- 
 	public Integer getNumberOfReviewsForGivenRatingRange(Product item, Double paramDouble1, Double paramDouble2) {
 		return getNumberOfReviewsForGivenRatingRange(getSession().getSessionContext(), item, paramDouble1,
 				paramDouble2);
